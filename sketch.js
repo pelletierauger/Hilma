998f2808a824057bb8871dd20657b19dd6a62f3f
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-04-05 01:39:40 -0400

    Make it a simple math visualization again, not a short animation

--- sketch.js
+++ sketch.js
@@ -1,59 +1,32 @@
 var looping = true;
-var exporting = true;
-
+var exporting = false;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(51);
-    // noLoop();
-    // noStroke();
-    // fill(255);
-    // noFill();
-    // fill(255, 50);
+    noFill();
     stroke(255);
     frameRate(30);
-
 }
 
 function draw() {
     background(51);
-    translate(width / 2, 0);
-    for (var j = 0; j < 20; j++) {
-        push();
-        translate(0, j * 20 + 350);
-        var mapFill = map(j, 0, 20, 40, 255);
-        var mapFill2 = map(j, 0, 20, 255, 40);
-        fill(mapFill);
-        stroke(mapFill2)
-        beginShape();
-        for (var i = 0; i < 10; i += 0.01) {
-            // var mapY = map(abs(i - width / 2), 0, 100, 0, 5);
-            var mapX = map(i, 0, 1000, 0, width);
-            var x = i - 5;
-            var y = -(1 / sqrt(TWO_PI)) * pow(2.71828, -pow(x, 2) / 2);
-            var n = noise(i + j / 10 + frameCount / 10) / 20;
-            n += noise(i + j / 10 + frameCount / 10) * 2 * y;
-            y += n;
-            y *= 300;
-            x = map(x, -5, 5, -width / 2 + 20, width / 2 - 20);
-
-            vertex(x, y + n);
-
-        }
-        if (j == 19) {
-            vertex(x, 50);
-            vertex(-x, 50);
-        } else {
-            vertex(x, y + n + 30);
-            vertex(-x, y + n + 30);
-        }
-
-        endShape(CLOSE);
-        // stroke(255, 10);
-        // line(0, height / 2, 0, -height / 2);
-        // line(width / 2, 0, -width / 2, 0);
-        pop();
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < 10; i += 0.01) {
+        // var mapY = map(abs(i - width / 2), 0, 100, 0, 5);
+        var mapX = map(i, 0, 1000, 0, width);
+        var x = i - 5;
+        var y = -(1 / sqrt(TWO_PI)) * pow(2.71828, -pow(x, 2) / 2);
+        var n = noise(i + frameCount / 10) / 20;
+        n += noise(i + frameCount / 10) * 2 * y;
+        y += n;
+        y *= 300;
+        x = map(x, -5, 5, -width / 2 + 20, width / 2 - 20);
+        vertex(x, y + n);
     }
+    endShape(OPEN);
+
     if (exporting) {
         frameExport();
     }
