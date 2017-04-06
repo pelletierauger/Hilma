faea410c619f060d4d4690a7920c049a2a6a4047
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-04-06 02:37:38 -0400

    Make a first fully working prototype of the Hilma graphite-drawing machine

--- sketch.js
+++ sketch.js
@@ -1,18 +1,116 @@
 var looping = true;
 var exporting = false;
+var arr = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(250);
+    background(255);
     // noFill();
     // stroke(255);
     noStroke();
+    // stroke(0, 30);
     frameRate(30);
-    // noLoop();
+    // arr = [createVector(0, 0),
+    //     createVector(10, 10),
+    //     createVector(30, 30),
+    //     createVector(50, 100),
+    //     createVector(100, 20),
+    //     createVector(130, 0)
+    // ];
+    noLoop();
     blendMode(DARKEST);
+    var total = 50;
+    var increment = TWO_PI / total;
+    for (var i = 0; i < TWO_PI + increment; i += increment) {
+        var x = cos(i) * 250;
+        var y = sin(i) * 250;
+        var vec = createVector(x, y);
+        arr.push(vec);
+    }
+
 }
 
 function draw() {
+    translate(width / 2, height / 2);
+    // scale(0.5, 0.5);
+    // drawGraphiteBasic();
+    drawGraphite(arr);
+    if (exporting) {
+        frameExport();
+    }
+}
+
+function drawGraphite(h) {
+    var drawCount = 0;
+    for (var j = 0; j < h.length - 1; j++) {
+        if (h[j + 1]) {
+            // stroke(255, 100);
+            // line(h[j].x, h[j].y, h[j + 1].x, h[j + 1].y);
+            // stroke(0, 30);
+
+            var v1 = h[j];
+            var hypotenuse = dist(h[j].x, h[j].y, h[j + 1].x, h[j + 1].y);
+            // var opposite = abs(h[j + 1].y - h[j].y);
+            // var sinus = opposite / hypotenuse;
+            // var angle = Math.asin(sinus);
+            var angle = atan2(h[j + 1].y - h[j].y, h[j + 1].x - h[j].x);
+
+            push();
+            translate(v1.x, v1.y);
+            rotate(angle + PI / 2);
+            // if (h[j + 1].y < h[j].y) {
+            //     rotate(-angle + (PI / 2));
+            // } else {
+            //     rotate(angle + (PI / 2));
+            // }
+            // stroke(0);
+            // line(-10, 0, 10, 0);
+            // fill(200);
+            // ellipse(0, 0, 5);
+
+            for (var i = 0; i < hypotenuse; i++) {
+                translate(0, -1);
+                drawGraphiteIsolated(drawCount);
+                drawCount++;
+            }
+            // ellipse(0, 0, 5);
+            pop();
+        }
+    }
+
+    //h is an array of vectors
+}
+
+function drawGraphiteIsolated(t) {
+    for (var i = 0; i < 10; i += 0.12) {
+        var x = i - 5;
+        var y = -(1 / sqrt(TWO_PI)) * pow(2.71828, -pow(x, 2) / 2);
+        var noiseValue = i + t / 100;
+        var n = noise(noiseValue) / 200;
+        n += noise(noiseValue) * 2 * y;
+        y += n;
+        y *= 300;
+        var n2 = noise(t / 50);
+        var n3 = noise(100 + t / 50);
+        var lineWidth = 6;
+        x *= lineWidth;
+        if (x <= 0) {
+            x *= n2;
+        } else {
+            x *= n3;
+        }
+        var mapCrayon = map(y, -200, 0, 80, 250);
+        mapCrayon = constrain(mapCrayon, 0, 250);
+        fill(round(mapCrayon));
+        push();
+        var nGrain = noise(t) * lineWidth * 1;
+        translate(random(nGrain), random(nGrain));
+        ellipse(x, 0, 2);
+        pop();
+    }
+}
+
+function drawGraphiteBasic() {
     // background(51);
 
     translate(width / 2, 0);
@@ -56,10 +154,6 @@ function draw() {
         // vertex(x, y + n);
     }
     // endShape(OPEN);
-
-    if (exporting) {
-        frameExport();
-    }
 }
 
 function keyPressed() {
